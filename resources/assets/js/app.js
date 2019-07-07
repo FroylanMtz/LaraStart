9708670529310bdd7d4fd7576e39ee57de509197
Froylan <1530006@upv.edu.mx>
2019-07-07 12:45:19 -0500

    Adicion de la parte de vform
    Esto se usa para validar los formularios.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,12 +9,20 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import { Form, HasError, AlertError } from 'vform'
+
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+window.form = Form;
+
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue') },
-    { path: '/profile', component: require('./components/Profile.vue') }
+    { path: '/profile', component: require('./components/Profile.vue') },
+    { path: '/users', component: require('./components/Users.vue') }
   ]
 
 
